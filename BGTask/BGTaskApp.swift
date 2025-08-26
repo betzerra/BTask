5d56459b9041c08df21f44d17389bfe5dcb23c94
Ezequiel Becerra <ezequiel.becerra@gmail.com>
2025-08-26 18:45:37 -0300

    Initial Commit

---
+++ BGTask/BGTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  BGTaskApp.swift
+//  BGTask
+//
+//  Created by Ezequiel Becerra on 26/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct BGTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
